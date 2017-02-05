a4c9b2de771d59381041c6fad174cdf1eb2da18d
Gerard Braad <me@gbraad.nl>
2017-02-06 01:51:02 +0800

    Remove duplication

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,8 +9,6 @@ if [ "$FIX_OWNERSHIP" != "" ]; then
 	chown -R `id -u`:0 /etc/asterisk /var/log/asterisk /var/run/asterisk /var/lib/asterisk /var/spool/asterisk
 fi
 
-echo "oc:x:`id -u`:0:oc:/:/sbin/nologin" >> /etc/passwd
-
 echo "[${STAMP}] Starting daemon..."
 # run asterisk daemon
 /usr/sbin/asterisk -vvv
